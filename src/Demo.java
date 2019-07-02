22c259e0d5fbb0fb9b4d614cc0402bff21fe6e67
oyushche <uhmateush@gmail.com>
2019-07-02 03:57:11 -0400

    create drink method

--- src/Demo.java
+++ src/Demo.java
@@ -2,28 +2,22 @@ public class Demo
 {
     public static void main(String[] args)
     {
-        Drink drink1 = new Drink();
-
-        drink1.setName("Coffee");
-        drink1.setPrice(40.00);
-
-        Drink drink2 = new Drink();
-
-        drink2.setName("Coffee");
-        drink2.setPrice(40.00);
-
-        Drink drink3 = new Drink();
-
-        drink3.setName("Latte");
-        drink3.setPrice(60.00);
+        Drink drink1 = createDrink("Coffee", 40.0);
+        Drink drink2 = createDrink("Latte", 60.0);
+        Drink drink3 = createDrink("Tea", 10.0);
 
         System.out.println(drink1);
         System.out.println(drink2);
         System.out.println(drink3);
+    }
 
-        System.out.println("does drink1 equal to drink2? " + drink1.equals(drink2));
-        System.out.println("does drink1 equal to drink3? " + drink1.equals(drink3));
+    public static Drink createDrink(String name, double price)
+    {
+        Drink drink = new Drink();
 
+        drink.setName(name);
+        drink.setPrice(price);
 
+        return drink;
     }
 }
