65fa38af3e72772fdbf94fb9c69caf0ef9c0db40
oyushche <uhmateush@gmail.com>
2019-07-02 04:18:10 -0400

    + Cafe + list to store data inside cafe + commentd

--- src/Cafe.java
+++ src/Cafe.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// general interface to use cafe
+// storing object
 public class Cafe
 {
     private List<Drink> drinkList;
@@ -12,22 +14,18 @@ public class Cafe
 
     public void createDrink(String name, double price)
     {
+        // 1. creates drink object
         Drink drink = new Drink();
 
         drink.setName(name);
         drink.setPrice(price);
 
+        // 2. store object to list
         drinkList.add(drink);
     }
 
     public void printMenu()
     {
-        for (Drink drink : drinkList)
-        {
-            System.out.println(drink);
-        }
-
-        // 2nd option
-//        System.out.println(drinkList);
+        System.out.println(drinkList);
     }
 }
